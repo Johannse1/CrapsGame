623d35adcafb0b1902fdd2b5a2f5758be2bd9604
Evan <johannse@hartwick.edu>
2019-10-31 09:04:45 -0400

    update

--- groupgame.py
+++ groupgame.py
@@ -13,9 +13,12 @@ def roll():
 def play_game():
     p1_points = 0
     p2_points = 0
-    while p1_points < 6 or p2_points
-    player_one = roll()
-    player_two = roll()
-    if
+    while p1_points < 6 or p2_points < 6:
+        player_one = roll()
+        player_two = roll()
+        print(f"player ones roll: {player_one}")
+        input("press enter to roll again:")
+    if player_one > player_two:
+        p1_points + 1
 
 
