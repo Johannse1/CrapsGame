b29bfdd8542d1a7e608c20935c94658ba0e84fe6
Evan <johannse@hartwick.edu>
2019-10-31 09:00:51 -0400

    update

--- groupgame.py
+++ groupgame.py
@@ -5,7 +5,17 @@
 import random
 
 
-def player_one():
-
+def roll():
+    num = random.randint(1,6)
     return
+
+
+def play_game():
+    p1_points = 0
+    p2_points = 0
+    while p1_points < 6 or p2_points
+    player_one = roll()
+    player_two = roll()
+    if
+
 
